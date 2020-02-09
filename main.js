9852f9c5aa38767a964c6e282729046353c50f65
yrevtovich <yrevtovich@mail.ru>
2020-02-09 23:42:42 +0300

    done

--- main.js
+++ main.js
@@ -2,16 +2,30 @@ let loginInput = document.querySelector('.login');
 let passwordInput = document.querySelector('.password');
 
 passwordInput.addEventListener('focusout', passwordValidation);
-
+loginInput.addEventListener('focusout', loginValidation);
 
 function passwordValidation() {
-    let regexp = /[0-9A-Z_]{6,}/gi;
-    let someValue = passwordInput.value;
-    console.log(someValue);
-    if (someValue.match(regexp) != null) {
-        console.log('im here');
+    let regexp = /[\w-]{6,}/g;
+    let passwordValue = passwordInput.value;
+
+    if (passwordValue.match(regexp) == passwordValue) {
         passwordInput.classList.add('green');
+        passwordInput.classList.remove('red');
     } else {
         passwordInput.classList.add('red');
+        passwordInput.classList.remove('green');
+    }
+}
+
+function loginValidation() {
+    let regexp = /[^\d]{1}[\w]*@[\w]+\.[\w]+/;
+    let loginValue = loginInput.value;
+
+    if (loginValue.match(regexp) == loginValue) {
+        loginInput.classList.add('green');
+        loginInput.classList.remove('red');
+    } else {
+        loginInput.classList.add('red');
+        loginInput.classList.remove('green');
     }
 }
